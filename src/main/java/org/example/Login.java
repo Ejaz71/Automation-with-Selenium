fbe404085f26a506f129a1323607d16fd0f9e183
Ejaz71 <ejazchowdhury.buetynwa@gmail.com>
2026-02-17 19:13:17 +0600

    added login with correct and incorrect creds automation, and forgot password automation

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,95 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Login {
+    WebDriver driver = new ChromeDriver();
+
+
+    String userNameLocator = "inputUsername"; //id
+    String passWordLocator = "inputPassword"; //name
+    String signInBtnLocator = "signInBtn"; //class
+    String errorMessageLocator = "error"; //clss
+    String rememberCheckboxLocator = ".checkbox-container #chkboxOne";
+    String tncCheckboxLocator = ".checkbox-container #chkboxTwo";
+    //Forgot password fields
+    String forgotPasswordLocator = "div.forgot-pwd-container a"; //css
+    String nameFieldLocator = "//input[@placeholder = 'Name']"; //xpath
+    String emailFieldLocator = "//input[@placeholder='Email']";
+    String phoneNumberLocator = "//form/input[3]"; //xpath
+    String resetLoginBtnLocator = "button.reset-pwd-btn"; //css
+    String infoMsgLocator = "form > p.infoMsg"; //css
+    String gotoLoginBtnLocator = ".go-to-login-btn";
+
+    // value fields
+    //login fields
+    String loginUserName = "Rahul";
+    String wrongPassword = "abc123";
+    String correctPassword = "rahulshettyacademy";
+    //forgot password form fields
+    String testUserName = "Test User";
+    String testUserEmail = "test.user@gmail.com";
+    String testUserPhoneNumber = "+8801718550000";
+
+    public void customerLoginWithIncorrectPass() {
+        //timeout set to 5s
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        driver.findElement(By.id(userNameLocator)).sendKeys(loginUserName);
+        driver.findElement(By.name(passWordLocator)).sendKeys(wrongPassword);
+        driver.findElement(By.className(signInBtnLocator)).click();
+
+
+        String s = driver.findElement(By.className(errorMessageLocator)).getText();
+        System.out.println(s);
+
+
+        //driver.close();
+    }
+
+    public void resetPassword() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        //driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.cssSelector(forgotPasswordLocator)).click();
+        driver.findElement(By.xpath(nameFieldLocator)).sendKeys(testUserName);
+        driver.findElement(By.xpath(emailFieldLocator)).sendKeys(testUserEmail);
+        driver.findElement(By.xpath(phoneNumberLocator)).sendKeys(testUserPhoneNumber);
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElement(By.cssSelector(resetLoginBtnLocator)).click();
+
+        String infoMessage = driver.findElement(By.cssSelector(infoMsgLocator)).getText();
+        System.out.println(infoMessage);
+
+        driver.findElement(By.cssSelector(gotoLoginBtnLocator)).click();
+        customerLoginWithCorrectPass();
+
+    }
+
+    public void customerLoginWithCorrectPass () {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        driver.findElement(By.id(userNameLocator)).sendKeys(loginUserName);
+        driver.findElement(By.name(passWordLocator)).sendKeys(correctPassword);
+
+        WebElement checkBoxOne = driver.findElement(By.cssSelector(rememberCheckboxLocator));
+        if (!checkBoxOne.isSelected()) checkBoxOne.click();
+
+        WebElement checkBoxTwo = driver.findElement(By.cssSelector(tncCheckboxLocator));
+        if (!checkBoxTwo.isSelected()) checkBoxTwo.click();
+
+        driver.findElement(By.className(signInBtnLocator)).click();
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.bidi.log.Log;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,31 +11,37 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Main {
     public static void main(String[] args) {
         //System.setProperty("webdriver.chrome.driver", "D:\\webdrivers\\Chrome_Webdriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //fireforx launch
-        WebDriver driverff = new FirefoxDriver();
-        driverff.get("https://google.com");
-        System.out.println(driverff.getTitle());
-        driverff.quit();
+//        WebDriver driver = new ChromeDriver();
+//        //fireforx launch
+//        WebDriver driverff = new FirefoxDriver();
+//        driverff.get("https://google.com");
+//        System.out.println(driverff.getTitle());
+//        driverff.quit();
+//
+//        //chrome
+//        driver.get("https://rahulshettyacademy.com/");
+//        // check title
+//        String title = driver.getTitle();
+//        System.out.println(title);
+//        //check URL
+//        System.out.println(driver.getCurrentUrl());
+//        driver.close();
+//
+//        int n = 1;
+//
+//        //edge
+//        WebDriver driverEdge = new EdgeDriver();
+//        try {
+//            driverEdge.get("https://google.com");
+//            System.out.println("This is edge title: " + driverEdge.getTitle());
+//        } finally {
+//            driverEdge.close();
+//        }
 
-        //chrome
-        driver.get("https://rahulshettyacademy.com/");
-        // check title
-        String title = driver.getTitle();
-        System.out.println(title);
-        //check URL
-        System.out.println(driver.getCurrentUrl());
-        driver.close();
-
-        //edge
-        WebDriver driverEdge = new EdgeDriver();
-        try {
-            driverEdge.get("https://google.com");
-            System.out.println("This is edge title: " + driverEdge.getTitle());
-        } finally {
-            driverEdge.close();
-        }
-
+        Login loginObj = new Login();
+        loginObj.customerLoginWithIncorrectPass();
+        loginObj.resetPassword();
+        loginObj.customerLoginWithCorrectPass();
 
     }
 }
